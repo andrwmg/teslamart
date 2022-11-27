623cd7e7f6bfde8e2f55df695dc5fff0e2e1d543
andrwmg <58545642+andrwmg@users.noreply.github.com>
2022-11-27 14:01:32 -0800

    Add image upload, user authetication, and comments

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ db.mongoose
 
 let corsOptions = {
   origin: 
-    // "https://teslamartv2.herokuapp.com",
+    "https://teslamartv2.herokuapp.com",
     // "https://teslamartv2backend.herokuapp.com",
-  "http://localhost:8081",
+  // "http://localhost:8081",
   credentials: true
 };
 app.use(bodyParser.urlencoded({extended: true}));
